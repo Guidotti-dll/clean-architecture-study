5241ddb29bf2fc1cbf9796970e254e555b0859b4
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-17 10:18:54 -0300

    refactor: add custumized error for missing param

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,16 +1,17 @@
+import { MissingParamError } from '../errors/missing-param-error'
 import { HttpRequest, httpResponse } from '../prottocols/http'
 
 export class SignUoController {
   handle (httpRequest: HttpRequest): httpResponse {
     if (!httpRequest.body.name) {
       return {
-        body: new Error('Missing param: name'),
+        body: new MissingParamError('name'),
         statusCode: 400
       }
     }
     if (!httpRequest.body.email) {
       return {
-        body: new Error('Missing param: email'),
+        body: new MissingParamError('email'),
         statusCode: 400
       }
     }
