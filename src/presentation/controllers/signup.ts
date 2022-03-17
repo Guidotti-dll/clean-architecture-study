71ffcf0e5776bef8dd38ea540e97c6c9b6e3a8bc
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-17 10:52:13 -0300

    feat: ensure SignUpController returns 400 if no passwordConfimation is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,7 @@ import { HttpRequest, httpResponse } from '../protocols/http'
 
 export class SignUoController {
   handle (httpRequest: HttpRequest): httpResponse {
-    const requiredFields = ['name', 'email', 'password']
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
