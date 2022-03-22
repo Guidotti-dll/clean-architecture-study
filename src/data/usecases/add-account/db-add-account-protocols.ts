959945bbb12d7dbed1eb6d3afe8525c6c4c7b384
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-22 10:59:47 -0300

    refactor: change file name

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../../domain/models/account'
 export * from '../../../domain/usecases/add-account'
-export * from '../../protolols/encrypter'
-export * from '../../protolols/add-account-repository'
+export * from '../../protocols/encrypter'
+export * from '../../protocols/add-account-repository'
--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,6 +1,6 @@
 
 import bcrypt from 'bcrypt'
-import { Encrypter } from '../../data/protolols/encrypter'
+import { Encrypter } from '../../data/protocols/encrypter'
 
 export class BcryptAdapter implements Encrypter {
   private readonly salt: number
