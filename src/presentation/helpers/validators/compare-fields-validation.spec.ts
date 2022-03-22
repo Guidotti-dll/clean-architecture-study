4ad657a6081ce21769002158a86dbecd481699b8
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-22 16:17:16 -0300

    test: add tests to CompareFieldsvalidation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,20 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('any_field', 'fieldToCompare')
+}
+
+describe('CompareField Validation', () => {
+  test('Should return a InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({ any_field: 'any_value', fieldToCompare: 'wrong_value' })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('Should not return a MissingParamErro if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ any_field: 'any_value', fieldToCompare: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})
