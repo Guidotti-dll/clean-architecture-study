127d86754c3e8c34324b7981f0aab00086a084b3
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-23 11:21:38 -0300

    test: ensure DbAuthentication throws if HashCoparer throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -80,4 +80,11 @@ describe('DbAuthentication UseCase', () => {
 
     expect(compareSpy).toHaveBeenCalledWith('any_password', 'hash_password')
   })
+
+  test('Should throw if HashComparer throws', async () => {
+    const { sut, hashComparerStub } = makeSut()
+    jest.spyOn(hashComparerStub, 'compare').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.auth(makeFakeAuthentication())
+    void expect(promise).rejects.toThrow()
+  })
 })
