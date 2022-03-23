9cc3ede4ed2056ff2e7d04a282f04cfb72a2d797
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-23 17:28:39 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/SignUp/signup-controller.ts
+++ src/presentation/controllers/SignUp/signup-controller.ts
@@ -3,9 +3,10 @@ import { badRequest, ok, serverError } from '../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, AddAccount, Validation } from './signup-controller-protocols'
 
 export class SignUpController implements Controller {
-  private readonly addAccount: AddAccount
-  private readonly validation: Validation
-  constructor (addAccount: AddAccount, validation: Validation) {
+  constructor (
+    private readonly addAccount: AddAccount,
+    private readonly validation: Validation
+  ) {
     this.addAccount = addAccount
     this.validation = validation
   }
