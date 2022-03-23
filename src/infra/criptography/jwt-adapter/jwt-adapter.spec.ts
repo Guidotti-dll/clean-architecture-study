858a1f0d1bef073c1cf092a8c43cde1211386190
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-23 14:48:10 -0300

    test: ensure JwtAdapter calls sign with corrects values

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken'
+import { JwtAdapter } from './jwt-adapter'
+
+describe('jwt Adapter', () => {
+  test('Should call sign with corrects values', async () => {
+    const sut = new JwtAdapter('secret')
+    const signSpy = jest.spyOn(jwt, 'sign')
+    await sut.encrypt('any_id')
+
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+})
