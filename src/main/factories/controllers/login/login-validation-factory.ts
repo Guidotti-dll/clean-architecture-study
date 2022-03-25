a7a08117e4e82a13134c51839e70059b0153e912
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-25 10:41:08 -0300

    refactor: move validation to its own layer

--- src/main/factories/controllers/login/login-validation-factory.ts
+++ src/main/factories/controllers/login/login-validation-factory.ts
@@ -1,7 +1,7 @@
 
 import { Validation } from '../../../../presentation/protocols/validation'
-import { ValidationComposite, RequiredFieldValidation, EmailValidation } from '../../../../presentation/helpers/validators'
-import { EmailValidatorAdapter } from '../../../adapters/validators/email-validator-adapter'
+import { ValidationComposite, RequiredFieldValidation, EmailValidation } from '../../../../validation/validators'
+import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator-adapter'
 
 export const makeLoginValidation = (): ValidationComposite => {
   const validations: Validation[] = []
--- src/main/factories/controllers/signup/signup-controller-factory..ts
+++ src/main/factories/controllers/signup/signup-controller-factory..ts
@@ -1,6 +1,6 @@
 import { SignUpController } from '../../../../presentation/controllers/SignUp/signup-controller'
 import { Controller } from '../../../../presentation/protocols'
-import { makeSignUpValidation } from './signup-validation-factory.'
+import { makeSignUpValidation } from './signup-validation-factory'
 import { makeDbAuthentication } from '../../usecases/authentication/db-auhentication-factory'
 import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory.'
 import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
