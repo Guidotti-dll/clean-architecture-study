e46885052b6998db2c17ba2129515fbb7db26da5
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-28 11:38:48 -0300

    feat: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository with correct values

--- src/data/usecases/load-account-by-token/db-load-account-by-token-protocols.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token-protocols.ts
@@ -1,4 +1,5 @@
 export * from '../../protocols/criptography/decrypter'
 export * from '../../../domain/usecases/load-account-by-token'
+export * from '../../protocols/db/account/load-account-by-token-repository'
 export * from '../add-account/db-add-account-protocols'
 export * from '../../../domain/models/account'
