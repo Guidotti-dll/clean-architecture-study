23c9cfbe8bf3dabccb25bf73d276e788a79ad61f
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-28 11:16:52 -0300

    feat: ensure DbLoadAccountByToken calls Decrypter with correct value

---
+++ src/data/usecases/load-account-by-token/db-load-account-by-token-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../protocols/criptography/decrypter'
+export * from '../../../domain/usecases/load-account-by-token'
+export * from '../add-account/db-add-account-protocols'
+export * from '../../../domain/models/account'
