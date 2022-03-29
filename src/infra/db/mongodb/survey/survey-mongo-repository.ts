a72692ab105aa85a8c7c2ccbe6842da6ef2501e6
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-29 18:35:03 -0300

    feat: ensure SurveyMongoRepository find survey by id with success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -3,8 +3,10 @@ import { SurveyModel } from '@/domain/models/survey'
 import { AddSurveyModel } from '@/domain/usecases/add-survey'
 import { AddSurveyRepository } from '@/data/protocols/db/survey/add-survey-repository'
 import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys-repository'
+import { LoadSurveyById } from '@/domain/usecases/load-survey-by-id'
+import { ObjectId } from 'mongodb'
 
-export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyById {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const accountCollection = await MongoHelper.getCollection('surveys')
     await accountCollection.insertOne(surveyData)
@@ -20,5 +22,11 @@ export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRe
     }
 
     return surveys
+  }
+
+  async loadById (id: string): Promise<SurveyModel> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const survey = await surveyCollection.findOne({ _id: new ObjectId(id) })
+    return MongoHelper.map(survey)
   }
 }
