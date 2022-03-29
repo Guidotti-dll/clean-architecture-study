515833c91176b6e6975d907abc19c6380661b913
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-29 09:34:23 -0300

    test: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -9,19 +9,27 @@ const makeSut = (): SurveyMongoRepository => {
   return new SurveyMongoRepository()
 }
 
-const makeSurveyData = (): AddSurveyModel => ({
+const makeSurveyData = (): AddSurveyModel[] => ([{
   question: 'any_question',
   answers: [
     {
       image: 'any_image',
       answer: 'any_answer'
-    },
+    }
+  ],
+  date: new Date()
+},
+{
+  question: 'other_question',
+  answers: [
     {
+      image: 'other_image',
       answer: 'other_answer'
     }
   ],
   date: new Date()
-})
+}
+])
 
 describe('Survey Mongo Repository', () => {
   beforeAll(async () => {
@@ -37,12 +45,24 @@ describe('Survey Mongo Repository', () => {
     await surveyCollection.deleteMany({})
   })
 
-  describe('add', () => {
+  describe('add()', () => {
     test('Should add a survey on add success', async () => {
       const sut = makeSut()
-      await sut.add(makeSurveyData())
+      await sut.add(makeSurveyData()[0])
       const survey = await surveyCollection.findOne({ question: 'any_question' })
       expect(survey).toBeTruthy()
+    })
+  })
+
+  describe('loadAll()', () => {
+    test('Should load all a survey on add success', async () => {
+      await surveyCollection.insertMany(makeSurveyData())
+
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
     })
   })
 })
