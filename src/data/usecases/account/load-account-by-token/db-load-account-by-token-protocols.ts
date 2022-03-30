7a42b11dea1dbab7df330b89e3590e03634a9c50
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-30 11:35:39 -0300

    refactor: organize project files

--- src/data/usecases/account/load-account-by-token/db-load-account-by-token-protocols.ts
+++ src/data/usecases/account/load-account-by-token/db-load-account-by-token-protocols.ts
@@ -1,5 +1,5 @@
-export * from '@/domain/usecases/load-account-by-token'
+export * from '@/domain/usecases/account/load-account-by-token'
 export * from '@/domain/models/account'
 export * from '@/data/protocols/criptography/decrypter'
 export * from '@/data/protocols/db/account/load-account-by-token-repository'
-export * from '@/data/usecases/add-account/db-add-account-protocols'
+export * from '@/data/usecases/account/add-account/db-add-account-protocols'
