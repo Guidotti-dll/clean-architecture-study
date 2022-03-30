58830a0f82d55a4a0b73de281243d79e7fb19900
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-30 10:55:39 -0300

    test: ensure SurveyResultMongoRepository updaets surveyResult if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -1,13 +1,12 @@
 import { SaveSurveyResultRepository, SaveSurveyResultModel, SurveyResultModel } from '@/data/usecases/save-survey-result/db-save-survey-result-protocols'
-import { ObjectId } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
   async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
     const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
     const surveyResult = await surveyResultCollection.findOneAndUpdate({
-      surveyId: new ObjectId(data.surveyId),
-      accountId: new ObjectId(data.accountId)
+      surveyId: data.surveyId,
+      accountId: data.accountId
     }, {
       $set: {
         answer: data.answer,
@@ -16,6 +15,7 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
     }, {
       upsert: true
     })
-    return { ...data, id: surveyResult.lastErrorObject?.upserted.toString() }
+
+    return { ...data, id: surveyResult.value?._id.toString() ?? surveyResult.lastErrorObject?.upserted.toString() }
   }
 }
