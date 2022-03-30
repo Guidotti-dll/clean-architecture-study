33b72aafb61449b83269b66e275893f261faefb0
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-30 14:23:21 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,15 +1,32 @@
 import { InvalidParamError } from '@/presentation/errors'
 import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
-import { Controller, HttpResponse, HttpRequest, LoadSurveyById } from './save-survey-result-controller-protocols'
+import { Controller, HttpResponse, HttpRequest, LoadSurveyById, SaveSurveyResult } from './save-survey-result-controller-protocols'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+      const { answer } = httpRequest.body
+      const { surveyId } = httpRequest.params
+      const { accountId } = httpRequest
+
+      const survey = await this.loadSurveyById.loadById(surveyId)
       if (survey) {
-        if (!survey.answers.find(answer => answer.answer === httpRequest.body.answer)) {
+        if (!survey.answers.find(_answer => _answer.answer === answer)) {
           return forbidden(new InvalidParamError('answer'))
+        }
+
+        if (accountId) {
+          await this.saveSurveyResult.save({
+            accountId,
+            surveyId,
+            answer,
+            date: new Date()
+          })
         }
       } else {
         return forbidden(new InvalidParamError('surveyId'))
