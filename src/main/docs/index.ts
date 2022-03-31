ad6038f3ced399f70d3d14815ab1a49342ace076
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-03-31 13:41:46 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Architecture Study',
+    description: 'API curso para estudo de arquitetura de software',
+    version: '1.0.0'
+  }
+}
