1cf16d7f2e3c124db9e7306ccad90f5ef8da79a9
Guidotti-dll <dev.lucasguidotti@gmail.com>
2022-04-01 16:28:12 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyRepository with correct values

---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -0,0 +1,16 @@
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
+
+export class DbLoadSurveyResult implements LoadSurveyResult {
+  constructor (
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository
+  ) {}
+
+  async load (surveyId: string): Promise<SurveyResultModel> {
+    console.log('surveyId: ', surveyId)
+
+    await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    return await Promise.resolve(null as unknown as SurveyResultModel)
+  }
+}
